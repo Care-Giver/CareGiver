2d3b085fa815815798c15a87f382694b7778e686
TomTimmy <70505699+TomTimmy@users.noreply.github.com>
2022-05-29 04:50:32 +0900

    CG-48 Feat: add script: update-common-images-index.ts
    common-images 폴더를 만들었다.
    위 스크립트는 자동으로 common-images 폴더에 추가된 image 들의 import, export default 를 업데이트 해준다. 스크립트 경로내에서 "node update-common-images-index.ts" 를 실행하면 작동한다

--- app/screens/home-stack/home/home-screen/home-screen.tsx
+++ app/screens/home-stack/home/home-screen/home-screen.tsx
@@ -17,7 +17,7 @@ import { ComeHomeGoToSwitchButton } from "../../../../custom-components/buttons/
 import { RowRoundedButton } from "../../../../custom-components/buttons/row-rounded-button/row-rounded-button"
 import { petsittersDummy, trainersDummy } from "./dummy-data"
 import { DotsIndicator } from "../../../../custom-components/dots-indicator/dots-indicator/dots-indicator"
-const gps = require("./images/gps.png")
+import images from "../../../../../assets/common-images"
 
 const FLATLIST_PADDING_VERTICAL = HEIGHT * 6 //? FlatList 내부의 있는 요소에 그림자가 있을 경우, FlatList 의 contentContainerStyle 에 padding 이 없을 경우, 그림자가 짤린다
 const FLATLIST_PADDING_HORIZONTAL = WIDTH * 10 //? ""
@@ -68,7 +68,7 @@ export const HomeScreen: FC<StackScreenProps<NavigatorParamList, "home">> = obse
     return (
       <ScreenRootView testID="HomeScreen" preset="scroll">
         <RowRoundedButton
-          image={gps}
+          image={images.gps}
           text={"경기 안산시 상록구 한양대학로 55"}
           textColor={BODY}
           onPress={() => {
---
+++ assets/common-images/index.ts
@@ -0,0 +1,13 @@
+//! 항상 이미지 파일명은 언더바 (_) 로 작성한다
+  const images = {
+  calendar: require('../common-images/calendar.png'),
+  empty_12: require('../common-images/empty_12.png'),
+  empty_heart: require('../common-images/empty_heart.png'),
+  gps: require('../common-images/gps.png'),
+  list_bars: require('../common-images/list_bars.png'),
+  location: require('../common-images/location.png'),
+  timer: require('../common-images/timer.png')
+}
+
+//! 예외적으로 export default 허용 
+export default images
