d6403fc9011c8b6ef6f11cdd086f6967cbcc9361
hycv <choehy31@gmail.com>
2022-07-06 23:08:35 +0900

    CG-112 Refactor: 변수명, 스타일 내 변수명 수정

--- app/custom-components/hired-times-and-pet-years/hired-times-and-pet-years.tsx
+++ app/custom-components/hired-times-and-pet-years/hired-times-and-pet-years.tsx
@@ -5,28 +5,28 @@ import { BODY, STRONG_LINE } from "../../theme/palette"
 import { styles } from "./styles"
 
 export const HiredTimesAndPetYears = () => {
-  const HIRED_TIMES = 99
-  const PET_YEARS_YEARS = 12
-  const PET_YEARS_MONTHS = "04"
+  const hiredTimes = 99
+  const petYearsYears = 12
+  const petYearsMonths = "04"
 
   return (
     //*전체 박스 컴포넌트
-    <View style={styles.ROW_ROUNDED_LARGE_BOX}>
+    <View style={styles.rowRoundedLargeBox}>
       {
         <>
           {/*divider 기준 왼쪽 텍스트*/}
-          <View style={styles.TEXT_ALIGNMENT}>
+          <View style={styles.textAlignment}>
             <PreMed14 color={BODY}>고용된 횟수</PreMed14>
-            <PreBol16 color={STRONG_LINE} text={HIRED_TIMES + "회"} style={{ marginTop: 8 }} />
+            <PreBol16 color={STRONG_LINE} text={hiredTimes + "회"} style={{ marginTop: 8 }} />
           </View>
           {/*세로 중간 divider*/}
-          <View style={styles.VERTICAL_DIVIDER}></View>
+          <View style={styles.verticalDivider}></View>
           {/* divider 기준 오른쪽 텍스트*/}
-          <View style={styles.TEXT_ALIGNMENT}>
+          <View style={styles.textAlignment}>
             <PreMed14 color={BODY}>반려동물과 함께한 시간</PreMed14>
             <PreBol16
               color={STRONG_LINE}
-              text={PET_YEARS_YEARS + "년" + PET_YEARS_MONTHS + "개월"}
+              text={petYearsYears + "년" + petYearsMonths + "개월"}
               style={{ marginTop: 8 }}
             />
           </View>
--- app/custom-components/hired-times-and-pet-years/styles.ts
+++ app/custom-components/hired-times-and-pet-years/styles.ts
@@ -4,7 +4,7 @@ import { WIDTH, HEIGHT, palette } from "../../theme"
 
 export const styles = StyleSheet.create({
   //*전체가 담기는 큰 컴포넌트 박스
-  ROW_ROUNDED_LARGE_BOX: {
+  rowRoundedLargeBox: {
     width: WIDTH * 358, //"100%",
     height: HEIGHT * 80,
     backgroundColor: palette.white,
@@ -18,7 +18,7 @@ export const styles = StyleSheet.create({
 
   //* 세로 두 줄의 텍스트를 묶는 스타일
 
-  TEXT_ALIGNMENT: {
+  textAlignment: {
     flexDirection: "column",
     alignItems: "center",
     flex: 1,
@@ -26,7 +26,7 @@ export const styles = StyleSheet.create({
 
   //* 중간 세로선 divider 스타일
 
-  VERTICAL_DIVIDER: {
+  verticalDivider: {
     width: WIDTH * 2,
     height: HEIGHT * 44,
     backgroundColor: LIGHT_LINE,
