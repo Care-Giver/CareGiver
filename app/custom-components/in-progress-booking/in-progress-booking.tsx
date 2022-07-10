788cdcb4075cf7d1e06c0d657980b69b21aae9d7
minSsan <msp1125@naver.com>
2022-07-10 18:54:16 +0900

    Setup in-progress-booking

---
+++ app/custom-components/in-progress-booking/in-progress-booking.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native"
+import React from "react"
+
+const InProgressBooking = () => {
+  return (
+    <View>
+      <Text>InProgressBooking</Text>
+    </View>
+  )
+}
+
+export default InProgressBooking
---
+++ app/screens/test/minseon-test.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native"
+import React from "react"
+
+const MinseonTest = () => {
+  return (
+    <View>
+      <Text>MinseonTest</Text>
+    </View>
+  )
+}
+
+export default MinseonTest
