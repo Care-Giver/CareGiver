66519bbdb1941d8c2d695329345fff987ea20752
TomTimmy <70505699+TomTimmy@users.noreply.github.com>
2022-07-14 11:26:11 +0900

    Chore: add common-styles.ts to screen-headers/

--- app/custom-components/screen-headers/writing-comment-screen-header/writing-comment-screen-header.tsx
+++ app/custom-components/screen-headers/writing-comment-screen-header/writing-comment-screen-header.tsx
@@ -1,27 +1,11 @@
-import { View, Image, Platform, Pressable } from "react-native"
+import { View, Image, Pressable } from "react-native"
 import React from "react"
-import { WIDTH, HEIGHT, HEADER_HEIGHT, IOS_NOTCH_STATUS_BAR_HEIGHT } from "../../../theme"
+import { WIDTH } from "../../../theme"
 import { PreMed20, PreBol16 } from "../../custom-texts/custom-texts"
 import IMAGES from "../../../../assets/common-images"
 import { styles } from "./styles"
 import { DISABLED, GIVER_CASUAL_NAVY } from "../../../theme/palette"
-
-const HEADER_ROOT = {
-  backgroundColor: "white",
-  flexDirection: "row",
-  alignItems: "center",
-  width: "100%",
-  height: Platform.select({
-    ios: HEADER_HEIGHT + IOS_NOTCH_STATUS_BAR_HEIGHT,
-    android: HEADER_HEIGHT,
-  }),
-
-  //! iOS 헤더 스타일링 개선: Status Bar 영역까지 헤더 컴포넌트가 있어야 함!
-  paddingTop: Platform.select({
-    ios: IOS_NOTCH_STATUS_BAR_HEIGHT,
-    android: 0,
-  }),
-}
+import { HEADER_ROOT } from "../common-styles"
 
 export const WritingCommentScreenHeader = (props) => {
   const title = props.options.title ? props.options.title : props.route.name
