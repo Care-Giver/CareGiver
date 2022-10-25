d608aac51c1f29024e08f420922f3df56954fa54
hycv <choehy31@gmail.com>
2022-10-26 01:39:01 +0900

    CG-179 Feat: 스타일링 1차 완료
    mst 작업 , 버튼 클릭시 화면 전환, 유저 info 불러오는 작업 필요

--- app/components/user-or-pet-profile-info/user-or-pet-profile-info.tsx
+++ app/components/user-or-pet-profile-info/user-or-pet-profile-info.tsx
@@ -14,7 +14,7 @@ import React, { FC, useLayoutEffect, useState } from "react"
 import { StackScreenProps } from "@react-navigation/stack"
 import { NavigatorParamList } from "#navigators"
 import { observer } from "mobx-react-lite"
-import { BODY, LBG, CARE_NATURAL_BLUE } from "#theme/palette"
+import { BODY, LBG, CARE_NATURAL_BLUE, HEAD_LINE, MIDDLE_LINE } from "#theme/palette"
 import {
   HEIGHT,
   WIDTH,
@@ -29,27 +29,25 @@ import {
   PopSem14,
   PopReg14,
   Row,
+  PreMed16,
   PreMed14,
   styles,
+  DivisionLine,
+  BASIC_BACKGROUND_PADDING_WIDTH,
 } from "#components"
 import { useKeyboard } from "@react-native-community/hooks"
 import IMAGES from "#images"
 
-export const UserOrPetProfileInfo = (title, moreInfoButtonAble, profileInfo) => {
-  return (
-    <View>
-      <PreMed14 color={BODY} text={title} style={{ paddingLeft: WIDTH * 16 }} />
+export const UserOrPetProfileInfo = (props) => {
+  //? props 를 이런 문법으로 쓰는 이유?
+  //? 아래와 같은 방뻐과 좀 다름 https://react.vlpt.us/basic/05-props.html
+  const { title, profileInfo } = props
 
-      <Pressable
-        onPress={() => {
-          alert("hi")
-        }}
-      >
-        <Image
-          source={IMAGES.more_info_bigger}
-          style={{ width: WIDTH * 16, height: HEIGHT * 16, marginLeft: WIDTH * 4 }}
-        />
-      </Pressable>
+  return (
+    <View style={{ paddingHorizontal: BASIC_BACKGROUND_PADDING_WIDTH, paddingTop: WIDTH * 20 }}>
+      <PreMed14 color={BODY} text={title} style={{ marginBottom: HEIGHT * 10 }} />
+      <PreMed16 color={HEAD_LINE} text={profileInfo} />
+      <DivisionLine color={MIDDLE_LINE} style={{ marginTop: HEIGHT * 4 }} />
     </View>
   )
 }
--- app/screens/mypage-stack/my-profile-management-screen/my-profile-management-screen.tsx
+++ app/screens/mypage-stack/my-profile-management-screen/my-profile-management-screen.tsx
@@ -14,7 +14,7 @@ import React, { FC, useLayoutEffect, useState } from "react"
 import { StackScreenProps } from "@react-navigation/stack"
 import { NavigatorParamList } from "#navigators"
 import { observer } from "mobx-react-lite"
-import { BODY, LBG, CARE_NATURAL_BLUE } from "#theme/palette"
+import { BODY, LBG, CARE_NATURAL_BLUE, HEAD_LINE, MIDDLE_LINE } from "#theme/palette"
 import {
   HEIGHT,
   WIDTH,
@@ -31,6 +31,10 @@ import {
   Row,
   PreMed14,
   styles,
+  PreMed16,
+  UserOrPetProfileInfo,
+  DivisionLine,
+  BASIC_BACKGROUND_PADDING_WIDTH,
 } from "#components"
 import { useKeyboard } from "@react-native-community/hooks"
 import IMAGES from "#images"
@@ -51,20 +55,37 @@ export const MyProfileManagementScreen: FC<
         }} //? width 를 곱하는것이 맞는지?
         source={IMAGES.my_profile_management_default}
       />
+      {/* //*닉네임 info 부분. 닉네임 옆의 more info 버튼으로 인해 컴포넌트로 이용하지 않음. 밑의 다른 info 들은 컴포넌트로 뺌.*/}
+      <View style={{ paddingHorizontal: BASIC_BACKGROUND_PADDING_WIDTH, marginTop: HEIGHT * 20 }}>
+        <Row style={{ marginBottom: HEIGHT * 10 }}>
+          <PreMed14 color={BODY} text={`닉네임`} />
+          <Pressable
+            onPress={() => {
+              alert("hi")
+            }}
+          >
+            <Image
+              source={IMAGES.more_info_bigger}
+              style={{ width: WIDTH * 16, height: HEIGHT * 16, marginLeft: WIDTH * 4 }}
+            />
+          </Pressable>
+        </Row>
+        <PreMed16 color={HEAD_LINE} text={`방울이엄마`} />
+        {/*//? marginRight 를 16으로 조절해야하는지? divisionline 을 적용시 디자인보다 오른쪽이 더 길어보임*/}
+        <DivisionLine color={MIDDLE_LINE} style={{ marginTop: HEIGHT * 4 }} />
+      </View>
 
-      <Row style={{ marginTop: 20 }}>
-        <PreMed14 color={BODY} text={`닉네임`} style={{ paddingLeft: WIDTH * 16 }} />
-        <Pressable
-          onPress={() => {
-            alert("hi")
-          }}
-        >
-          <Image
-            source={IMAGES.more_info_bigger}
-            style={{ width: WIDTH * 16, height: HEIGHT * 16, marginLeft: WIDTH * 4 }}
-          />
-        </Pressable>
-      </Row>
+      {/* //* 생년월일 */}
+      <UserOrPetProfileInfo title={"생년월일"} profileInfo={"99.12.28"} />
+
+      {/* //* 성별 */}
+      <UserOrPetProfileInfo title={"성별"} profileInfo={"여"} />
+
+      {/* //* 이메일 */}
+      <UserOrPetProfileInfo title={"이메일"} profileInfo={"hhh@gmail.com"} />
+
+      {/* //* 전화번호우 */}
+      <UserOrPetProfileInfo title={"전화번호"} profileInfo={"010-0000-0000"} />
     </ScreenRootView>
   )
 })
