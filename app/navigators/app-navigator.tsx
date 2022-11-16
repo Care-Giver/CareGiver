94e51fbfe5747122abe6c8de06449e046e17b89b
TomTimmy <70505699+TomTimmy@users.noreply.github.com>
2022-11-17 00:50:23 +1300

    UPDATE: add linking config

--- app/navigators/app-navigator.tsx
+++ app/navigators/app-navigator.tsx
@@ -283,11 +283,40 @@ interface NavigationProps extends Partial<React.ComponentProps<typeof Navigation
 export const AppNavigator = (props: NavigationProps) => {
   const colorScheme = useColorScheme()
   useBackButtonHandler(canExit)
+
+  const linking = {
+    // prefixes: ["https://mychat.com", "mychat://"],
+    config: {
+      screens: {
+        "home-screen": "/home-screen",
+        "search-screen": "/search-screen",
+        "search-result": "/search-result",
+        "caregiver-detail-information-screen": "/caregiver-detail-information-screen",
+        "all-reviews-screen": "/all-reviews-screen",
+        "caregiver-self-introduction-screen": "/caregiver-self-introduction-screen",
+        "all-comments-screen": "/all-comments-screen",
+        "writing-comment-screen": "/writing-comment-screen",
+        "all-bookings-screen": "/all-bookings-screen",
+        "booking-detail-screen": "/booking-detail-screen",
+
+        // * pay stack
+        "payment-request-screen": "/payment-request-screen",
+
+        // * mypage stack
+        "mypage-screen": "/mypage-screen",
+        "all-pets-screen": "/all-pets-screen",
+        "setting-screen": "/setting-screen",
+        "service-center-screen": "/service-center-screen",
+      },
+    },
+  }
+
   return (
     <NavigationContainer
       ref={navigationRef}
       theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
       {...props}
+      linking={linking}
     >
       <AppStack />
     </NavigationContainer>
