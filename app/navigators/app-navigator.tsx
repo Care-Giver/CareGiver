7bd264d06cd0e1887a6886224291ee7f6e24f54f
TomTimmy <70505699+TomTimmy@users.noreply.github.com>
2022-11-28 05:25:20 +1300

    Hotfix: refresh 하면, 404 에러뜨는거 수정하려했으나, 실패. - linking config 수정
    - vercel 자체의 문제인가?
    - 일단 config 수정했더니, url 자체가 더 깔끔해졌긴 함

--- app/navigators/app-navigator.tsx
+++ app/navigators/app-navigator.tsx
@@ -423,7 +423,7 @@ export const AppNavigator = (props: NavigationProps) => {
   useBackButtonHandler(canExit)
 
   const linking = {
-    // prefixes: ["https://mychat.com", "mychat://"],
+    prefixes: ["https://care-giver-web.vercel.app"],
     config: {
       screens: {
         "home-screen": "/",
@@ -448,6 +448,33 @@ export const AppNavigator = (props: NavigationProps) => {
       },
     },
   }
+
+  // const linking = {
+  //   prefixes: ["https://care-giver-web.vercel.app"],
+  //   config: {
+  //     screens: {
+  //       "home-screen": "/",
+  //       "search-screen": "/search-screen",
+  //       "search-result": "/search-result",
+  //       "caregiver-detail-information-screen": "/caregiver-detail-information-screen",
+  //       "all-reviews-screen": "/all-reviews-screen",
+  //       "caregiver-self-introduction-screen": "/caregiver-self-introduction-screen",
+  //       "all-comments-screen": "/all-comments-screen",
+  //       "writing-comment-screen": "/writing-comment-screen",
+  //       "all-bookings-screen": "/all-bookings-screen",
+  //       "booking-detail-screen": "/booking-detail-screen",
+
+  //       // * pay stack
+  //       "payment-request-screen": "/payment-request-screen",
+
+  //       // * mypage stack
+  //       "mypage-screen": "/mypage-screen",
+  //       "all-pets-screen": "/all-pets-screen",
+  //       "setting-screen": "/setting-screen",
+  //       "service-center-screen": "/service-center-screen",
+  //     },
+  //   },
+  // }
 
   return (
     <NavigationContainer
