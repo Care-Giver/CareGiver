595348fe527ed81e71ec77e92cb8de1e1a0b5da4
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2023-02-20 22:59:44 +1300

    (교육용) 함수를 prop으로 줄때 type 은?

--- app/components/buttons/register-submit-button/register-submit-button.props.ts
+++ app/components/buttons/register-submit-button/register-submit-button.props.ts
@@ -2,5 +2,14 @@ export interface RegisterSubmitButtonProps {
   isActive?: boolean
   text: string
   // ! 함수 타입 임시로 작성
-  onPress?: () => any
+  onPress?: () => void
+
+  //* 아래 코드는 교육용입니다 ^^
+  // addition: (a: number, b: number)  => number
+  // useState: (state: string) => void
 }
+
+// const addition = (a: number, b: number) => {
+//   const added = a + b
+//   return added
+// }
