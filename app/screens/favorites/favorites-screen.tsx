551a784a6bc3d0aa3c665663e9b41954c470ee8b
minSsan <msp1125@hanyang.ac.kr>
2023-04-03 21:05:33 +0900

    CG-245 Feat: favorites-screen 초기 세팅

--- app/screens/favorites/favorites-screen.tsx
+++ app/screens/favorites/favorites-screen.tsx
@@ -1,22 +1,98 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
+import { View, Image, Pressable } from "react-native"
 import { observer } from "mobx-react-lite"
 import { StackScreenProps } from "@react-navigation/stack"
 import { NavigatorParamList } from "#navigators"
-import { ScreenRootView } from "#components"
+import {
+  BASIC_BACKGROUND_PADDING_WIDTH,
+  DivisionLine,
+  PreBol18,
+  PreMed16,
+  PreReg12,
+  Row,
+  ScreenRootView,
+  ServiceTypeIndicatorHeader,
+} from "#components"
+import { styles } from "./styles"
+import { images } from "#images"
+import BottomSheet from "@gorhom/bottom-sheet"
+import { DISABLED } from "#theme"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
 
 export const FavoritesScreen: FC<
   StackScreenProps<NavigatorParamList, "favorites-screen">
 > = observer(function FavoritesScreen() {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
+  const [serviceType, setServiceType] = useState<string>("펫시터")
+  const [bottomSheetIndex, setBottomSheetIndex] = useState<number>(-1)
 
-  // Pull in navigation via hook
-  // const navigation = useNavigation()
+  console.log("bottomSheetIndex: ", bottomSheetIndex)
+
   return (
-    <ScreenRootView testID="Favorites" preset="scroll">
-      {/* 해피코딩^^ */}
-    </ScreenRootView>
+    <GestureHandlerRootView style={{ height: "100%" }}>
+      <ScreenRootView testID="Favorites" preset="scroll">
+        {/* //* 펫시터 | 훈련사 토글 */}
+        <Row style={{ marginTop: 24 }}>
+          <ServiceTypeIndicatorHeader
+            label={"펫시터"}
+            onPress={() => setServiceType("펫시터")}
+            state={serviceType}
+          />
+          <ServiceTypeIndicatorHeader
+            label={"훈련사"}
+            onPress={() => setServiceType("훈련사")}
+            state={serviceType}
+          />
+        </Row>
+
+        {/* //* filter box */}
+        <Row style={styles.filterBox}>
+          <PreReg12 text="전체" />
+          <Pressable
+            onPress={() => setBottomSheetIndex(0)}
+            style={{
+              flexDirection: "row",
+            }}
+          >
+            <PreReg12 text="원하는 조건으로 보기" />
+            <Image source={images.list_bars} style={styles.filterImg} />
+          </Pressable>
+        </Row>
+
+        {/* //* division line */}
+        <View
+          style={[styles.divisionLine, { marginHorizontal: -1 * BASIC_BACKGROUND_PADDING_WIDTH }]}
+        />
+      </ScreenRootView>
+
+      {/* //* 바텀시트 bottomSheet */}
+      <BottomSheet
+        index={bottomSheetIndex}
+        snapPoints={["72.9%"]}
+        enablePanDownToClose
+        style={{ backgroundColor: "red" }}
+      >
+        <View style={styles.bottomSheetContainer}>
+          <Row style={styles.bottomSheetTitleBox}>
+            <Pressable
+              onPress={() => {
+                console.log("clicked")
+                setBottomSheetIndex(-1)
+              }}
+            >
+              <Image source={images.x_grey} style={{ width: 16, height: 16 }} />
+            </Pressable>
+            <PreBol18 text="필터" />
+            <Pressable>
+              <PreMed16 text="초기화" color={DISABLED} />
+            </Pressable>
+          </Row>
+
+          {/* //* division line */}
+          <View style={styles.divisionLine} />
+        </View>
+      </BottomSheet>
+    </GestureHandlerRootView>
   )
 })
--- app/screens/favorites/styles.ts
+++ app/screens/favorites/styles.ts
@@ -1,3 +1,31 @@
+import { BASIC_BACKGROUND_PADDING_WIDTH } from "#components"
+import { LBG } from "#theme"
 import { StyleSheet } from "react-native"
 
-export const styles = StyleSheet.create({})
+export const styles = StyleSheet.create({
+  filterBox: {
+    paddingVertical: 12,
+    justifyContent: "space-between",
+  },
+
+  filterImg: {
+    width: 16,
+    height: 16,
+
+    marginLeft: 5,
+  },
+
+  divisionLine: {
+    backgroundColor: LBG,
+    height: 2,
+  },
+
+  bottomSheetContainer: {
+    paddingHorizontal: BASIC_BACKGROUND_PADDING_WIDTH,
+  },
+
+  bottomSheetTitleBox: {
+    paddingVertical: 12,
+    justifyContent: "space-between",
+  },
+})
