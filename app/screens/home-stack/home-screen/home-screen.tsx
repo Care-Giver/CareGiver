4e82ca408fd14e97e68ab3ba97267c701897ee89
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2023-04-30 00:38:16 +1200

    Add axios folder and setup absolute path

--- app/screens/home-stack/home-screen/home-screen.tsx
+++ app/screens/home-stack/home-screen/home-screen.tsx
@@ -18,7 +18,7 @@ import { navigate, NavigatorParamList } from "#navigators"
 import { BODY, SUB_HEAD_LINE } from "#theme"
 import { petsittersDummy, trainersDummy } from "./dummy-data"
 import { images } from "#images"
-import { getPosts } from "#api"
+import { getPosts } from "#axios"
 
 const FLATLIST_PADDING_VERTICAL = 6 //? FlatList 내부의 있는 요소에 그림자가 있을 경우, FlatList 의 contentContainerStyle 에 padding 이 없을 경우, 그림자가 짤린다
 const FLATLIST_PADDING_HORIZONTAL = 10 //? ""
