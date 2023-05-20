3ca11a014b8b121b1890da66c142d9b839ab50b2
hojkim77 <rlaghwnd4534@gmail.com>
2023-05-20 22:23:26 +0900

    MVP-6 (작업중) 모달 컴포넌트 통합 style 수정

--- app/components/custom-modal/custom-modal.tsx
+++ app/components/custom-modal/custom-modal.tsx
@@ -28,8 +28,8 @@ export const CustomModal = observer(function CustomModal(props: CustomModalProps
         >
           <Image source={image} style={{ width: 66, height: 66 }} />
           <PreBol20 text={title} color={HEAD_LINE} style={{ marginTop: 16 }} />
-          {subtitle && <PreReg14 text={subtitle} color={BODY} style={{ marginTop: 8 }} />}
-          <Row style={{ marginTop: 48 }}>
+          {subtitle && <PreReg14 text={subtitle} color={BODY} style={{ marginTop: 8, textAlign:"center" }} />}
+          <Row style={{ marginTop: 60 }}>
             <Pressable style={styles.modalYesBtn} onPress={handleYesPress}>
               <PreBol14 text={yesBtnText} color={GIVER_CASUAL_NAVY} />
             </Pressable>
