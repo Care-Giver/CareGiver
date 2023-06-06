90dab6ba21a485b3a94a09e84d6cc49d37f71051
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2023-06-07 01:08:55 +1200

    MVP-16 [코드리뷰] weight-modal 리팩토링 - react-native-modal 패키지 사용함

--- app/components/weight-modal/styles.ts
+++ app/components/weight-modal/styles.ts
@@ -2,6 +2,7 @@ import { DEVICE_SCREEN_WIDTH, LIGHT_LINE, palette } from "#theme"
 import { StyleSheet } from "react-native"
 import { BASIC_BACKGROUND_PADDING_WIDTH } from "#components"
 export const styles = StyleSheet.create({
+  root: { alignSelf: "center" },
   modalPetWeight: {
     width: DEVICE_SCREEN_WIDTH - 2 * BASIC_BACKGROUND_PADDING_WIDTH,
     paddingTop: 36,
@@ -10,7 +11,5 @@ export const styles = StyleSheet.create({
     height: 226,
     borderRadius: 8,
     backgroundColor: palette.white,
-    marginBottom: 60,
   },
-  divisionLine: { marginTop: 4, marginBottom: 4 },
 })
