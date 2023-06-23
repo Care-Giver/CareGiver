db0e7c11a5c9272289c3090ac15e28864297294c
beom-i <lybgreen77@gmail.com>
2023-06-24 01:40:13 +0900

    Revert "MVP-23 [작업중] 플랫폼마다 Bottom Bar Height 값 수정"
    This reverts commit 8497f98223004168e9b2179d2279cdc8d6d529eb.

--- app/components/click-to-blue-button/click-to-blue-button.tsx
+++ app/components/click-to-blue-button/click-to-blue-button.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import { StyleProp, TouchableOpacity, ViewStyle, View, StyleSheet } from "react-native"
 import { observer } from "mobx-react-lite"
 import { PreMed14, PreReg14 } from "#components"
@@ -10,45 +10,36 @@ export interface ClickToBlueButtonProps {
    */
   style?: StyleProp<ViewStyle>
 
-  isActiving: boolean
-
   buttonText: string
 
   buttonHeight: number
 
   buttonWidth: number
-
-  onPress?: () => void
 }
 
 export const ClickToBlueButton = observer(function ClickToBlueButton(
   props: ClickToBlueButtonProps,
 ) {
-  const { style, buttonText, buttonHeight, buttonWidth, isActiving, onPress } = props
+  const { style, buttonText, buttonHeight, buttonWidth } = props
   const allStyles = Object.assign({}, style)
 
-  const [isActive, setIsActive] = useState(isActiving)
+  const [isActive, setIsActive] = useState(false)
 
-  const handlePress = () => {
-    if (onPress) {
-      onPress() // onPress prop이 정의되어 있을 때 실행
-    } else {
-      // 기본 동작 정의
-      setIsActive(!isActive)
-    }
+  const handleClick = () => {
+    setIsActive(!isActive)
   }
 
   const buttonStyle = {
-    borderColor: isActiving ? GIVER_CASUAL_NAVY : DISABLED,
+    borderColor: isActive ? GIVER_CASUAL_NAVY : DISABLED,
   }
 
   return (
     <View style={allStyles}>
       <TouchableOpacity
         style={[styles.button, buttonStyle, { height: buttonHeight, width: buttonWidth }]}
-        onPress={handlePress}
+        onPress={handleClick}
       >
-        {isActiving ? (
+        {isActive ? (
           <PreMed14 text={buttonText} color={GIVER_CASUAL_NAVY} />
         ) : (
           <PreReg14 text={buttonText} color={DISABLED} />
