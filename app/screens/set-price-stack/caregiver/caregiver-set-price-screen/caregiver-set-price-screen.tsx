4b3f80af8cd736ac9737d00f85819d8057547fc5
hyeri1126 <hyeri1126@ewhain.net>
2023-08-15 18:56:33 +0900

    MVP-39 (작업중) caregiver-set-additional-screen 컴포넌트로 분리 후, caregiver-set-price-screen FlatList 내부에서 사용. (caregiver-set-additional-screen은 삭제해도 됨)

--- app/screens/set-price-stack/caregiver/caregiver-set-price-screen/caregiver-set-price-screen.tsx
+++ app/screens/set-price-stack/caregiver/caregiver-set-price-screen/caregiver-set-price-screen.tsx
@@ -1,13 +1,33 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
 import { StackScreenProps } from "@react-navigation/stack"
 import { NavigatorParamList, navigate } from "#navigators"
 import { observer } from "mobx-react-lite"
-import { CaregiverService, CaregiverSetPrice, GoBackSaveNext, PreReg12, Screen } from "#components"
-import { Pressable } from "react-native"
+import {
+  CaregiverService,
+  CaregiverSetPrice,
+  GoBackSaveNext,
+  PreReg12,
+  Screen,
+  CaregiverSetAdditionalPrice,
+} from "#components"
+import { Dimensions, FlatList, Pressable } from "react-native"
 
 export const CaregiverSetPriceScreen: FC<
   StackScreenProps<NavigatorParamList, "caregiver-set-price-screen">
 > = observer(({ route, navigation }) => {
+  const { width } = Dimensions.get("window")
+  const [itemWidth, setItemWidth] = useState(0)
+
+  const DATA = [
+    {
+      id: "bd7acbea",
+      title: "page1",
+    },
+    {
+      id: "bd7awwdd",
+      title: "page2",
+    },
+  ]
   const onPressGoback = () => {
     alert("이전 버튼 클릭")
     // 이전버튼 클릭시, 이전 컴포넌트로 스크롤 이동
@@ -18,8 +38,26 @@ export const CaregiverSetPriceScreen: FC<
   }
 
   return (
-    <Screen>
-      <CaregiverSetPrice />
+    <Screen style={{ flex: 1 }}>
+      {/* <CaregiverSetPrice /> */}
+      <FlatList
+        horizontal
+        // snapToInterval={width}
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ width: "200%" }}
+        data={DATA}
+        onContentSizeChange={(w) => setItemWidth(w / 2)}
+        renderItem={(item) => (
+          <>
+            {item.item.title === "page1" && <CaregiverSetPrice style={{ width: itemWidth }} />}
+            {item.item.title === "page2" && (
+              <CaregiverSetAdditionalPrice style={{ width: itemWidth }} />
+            )}
+          </>
+        )}
+        keyExtractor={(item) => item.id}
+      />
 
       {/* // * 다음 button */}
       {/* // TODO: onPress */}
