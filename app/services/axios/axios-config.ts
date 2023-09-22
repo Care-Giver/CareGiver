344264a960d7173b9df43cb681f7d5ddcc205abd
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2023-09-23 11:59:50 +1200

    MVP-32 (axios-config.ts) CONFIG 변수에 들어갈 x-jwt 토큰 값을 AsyncStorage 에서 추출할 함수 getToken 추가 함 (테스트 완료)

--- app/services/axios/axios-config.ts
+++ app/services/axios/axios-config.ts
@@ -1,3 +1,20 @@
+import { ROOT_STATE_STORAGE_KEY } from "#models"
+import { load } from "../../utils/storage"
+
+/**
+ * MST 를 통해 AsyncStorage 에 저장된, 유저 토큰 (이 값은 "x-jwt" 값으로 사용된다.) 을 가져온다.
+ * 만약 토큰이 없다면, false 를 반환한다.
+ *
+ * @returns {Promise<string | false>} token
+ */
+const getToken = async (): Promise<string | false> => {
+  const MSTRootStore = await load(ROOT_STATE_STORAGE_KEY)
+  console.log("token >>> ♦️", MSTRootStore.userStore.userAuth.token)
+
+  const token = MSTRootStore.userStore.userAuth.token
+  return token || false
+}
+
 export const BASE_URL = "http://ec2-3-36-101-9.ap-northeast-2.compute.amazonaws.com:3000/api/v1"
 
 // 유저 (userId: 7) 토큰
