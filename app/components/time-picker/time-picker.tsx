221601f06f4b6a5781ea812f9b8a86d6a146b5a5
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2023-09-25 04:37:00 +1300

    MVP-48 (time-picker) 끝 시간 핸들링 로직 오류 수정 (NOTE 참고) (NOTE) - 1시간 더해진 값으로 정해진 끝 시간이, 오늘을 넘어갈 경우, 두 로직을 순환하기때문에 무한 useEffect 콜이 일어 났었음. - 따라서, 조건문을 수정함

--- app/components/time-picker/time-picker.tsx
+++ app/components/time-picker/time-picker.tsx
@@ -1,9 +1,9 @@
 /**
  * DatePicker 는 기본적으로, 디바이스의 시간대를 기준으로 시간을 표시한다.
  * 그러나, 이미 부모 컴포넌트에서 디바이스 시간대를 기준으로 연산하여
- * props 를 넘겨주기 때문에, DatePicker 의 기본 설정은 이중 연사를 하게 된다.
+ * props 를 넘겨주기 때문에, DatePicker 의 기본 설정은 이중 연산을 하게 된다.
  * 따라서, timeZoneOffsetInMinutes 를 0 으로 설정하여,
- * DatePicker 컴포넌트가 UTC+0 을 기준으로 연산하도록 변경한다.
+ * DatePicker 컴포넌트가 UTC+0 을 기준으로 연산하도록 변경하여, 이중 연산을 방지한다.
  */
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react"
 import { View, StyleSheet, ViewStyle, StyleProp, Pressable } from "react-native"
@@ -38,22 +38,28 @@ export const TimePicker = observer(function TimePicker(props: TimePickerProps) {
 
   const [mode, setMode] = useState<Mode>("BEGIN")
 
-  // 선택한 시간이 바뀔때마다 핸들링
+  console.log("endDate", endDate)
+
+  /** 선택한 시간이 바뀔때마다 핸들링 */
   useEffect(() => {
     const oneHourAfterBeginDate = dayjs(beginDate).clone().add(1, "hour").toDate()
-    console.log("oneHourLaterFromBegin", oneHourAfterBeginDate)
 
-    // 끝 시간이
-    // "시작시간보다 이전" 이거나,
-    // "시작시간보다 1시간 이후보다 이전"이면,
-    // 끝 시간을 시작시간보다 1시간 이후로 강제
-    if (endDate <= beginDate || endDate < oneHourAfterBeginDate) {
-      setEndDate(oneHourAfterBeginDate)
-    }
-
-    // 끝 시간이 오늘을 넘어가면, 끝시간으로 오늘 23:55 를 강제
+    /** 끝 시간이 오늘을 넘어가면, 끝시간으로 오늘 23:55 를 강제 */
     if (endDate > localTimeEndOfToday) {
       setEndDate(setMinutes(localTimeEndOfToday, 55))
+      //
+    } else if (
+      /**
+       * 끝 시간이
+       * "시작시간보다 이전" 이거나,
+       * "시작시간보다 1시간 이후보다 이전"이면,
+       * 끝 시간을 시작시간보다 1시간 이후로 강제
+       */
+      endDate <= beginDate ||
+      (endDate < oneHourAfterBeginDate && endDate.getHours() !== 23 && endDate.getMinutes() !== 55)
+    ) {
+      setEndDate(oneHourAfterBeginDate)
+      //
     }
   }, [beginDate, endDate])
 
