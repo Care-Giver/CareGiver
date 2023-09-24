54feb932aaf7d98a210c15228ef133b0b4ab8943
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2023-09-25 03:02:42 +1300

    MVP-48 (react-native-calendars 패키지 관련) 달력 설정 오류 교정 -  프랑스 locale 값으로 잘못 선언 됨, 작동에는 문제없으나, 오해 여지가 있어서 한국(ko) 으로 변경. 그리고, 일주일 정렬 순서가 잘못 설정되어있었음. "일요일" 부터 시작했어야 함.

--- app/components/client-calendar/localeConfig.js
+++ app/components/client-calendar/localeConfig.js
@@ -1,6 +1,7 @@
 import { LocaleConfig } from "react-native-calendars"
 
-LocaleConfig.locales["fr"] = {
+// eslint-disable-next-line dot-notation
+LocaleConfig.locales["ko"] = {
   monthNames: [
     "1월",
     "2월",
@@ -29,8 +30,8 @@ LocaleConfig.locales["fr"] = {
     "11월",
     "12월",
   ],
-  dayNames: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
-  dayNamesShort: ["월", "화", "수", "목", "금", "토", "일"],
-  today: "Aujourd'hui",
+  dayNames: ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"],
+  dayNamesShort: ["일", "월", "화", "수", "목", "금", "토"],
+  today: "오늘",
 }
-LocaleConfig.defaultLocale = "fr"
+LocaleConfig.defaultLocale = "ko"
