5dd1db43526fbd15324d8aa263e13af91c7eb686
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2023-11-10 01:49:41 +0900

    Update(cg-calendar-screen): 작은 화면 핸들링을 위해 ScrollView 추가

--- app/screens/cg-calendar/cg-calendar-screen.tsx
+++ app/screens/cg-calendar/cg-calendar-screen.tsx
@@ -11,7 +11,7 @@ import {
   PreBol18,
 } from "#components"
 import { useStores } from "../../models"
-import { Pressable, View, Image } from "react-native"
+import { Pressable, View, Image, ScrollView } from "react-native"
 import { BODY, GIVER_CASUAL_NAVY } from "#theme"
 import {
   GroupedVisitingAvailableTimesByDate,
@@ -93,99 +93,107 @@ export const CgCalendarScreen: FC<
 
   return (
     <Screen testID="CgCalendar">
-      {hasPetsitterProfile ? (
-        <>
-          <PreBol18
-            text={`${serviceTypeKorean} 일정 관리`}
-            style={{ alignSelf: "flex-start" }}
-            mt={10}
-          />
-          <CancelButton
-            title={"전체해제"}
-            textcolor={BODY}
-            style={{ alignSelf: "flex-end", marginBottom: 8 }}
-            onPress={() => {
-              setSelectedDates([])
-            }}
-          />
-          <CgCalendar
-            availableDates={serviceTypeKorean === "방문" ? visitingAvailableTimes : crecheDates}
-            serviceTypeKorean={serviceTypeKorean}
-            selectedDates={selectedDates}
-            setSelectedDates={setSelectedDates}
-            style={{
-              alignSelf: "center",
-            }}
-          />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={{ flex: 1 }}
+        contentContainerStyle={{ flex: 1 }}
+      >
+        {hasPetsitterProfile ? (
+          <>
+            <PreBol18
+              text={`${serviceTypeKorean} 일정 관리`}
+              style={{ alignSelf: "flex-start" }}
+              mt={10}
+            />
+            <CancelButton
+              title={"전체해제"}
+              textcolor={BODY}
+              style={{ alignSelf: "flex-end", marginBottom: 8 }}
+              onPress={() => {
+                setSelectedDates([])
+              }}
+            />
+            <CgCalendar
+              availableDates={serviceTypeKorean === "방문" ? visitingAvailableTimes : crecheDates}
+              serviceTypeKorean={serviceTypeKorean}
+              selectedDates={selectedDates}
+              setSelectedDates={setSelectedDates}
+              style={{
+                alignSelf: "center",
+              }}
+            />
 
-          {/* 수정 버튼 */}
-          <ConditionalButton
-            label={"수정"}
-            isActivated={true}
-            style={{
-              alignSelf: "center",
-              position: "absolute",
-              bottom: BOTTOM_TAB_BAR_HEIGHT + 16,
-              backgroundColor: "white",
-              borderColor: GIVER_CASUAL_NAVY,
-              borderWidth: 2,
-            }}
-            labelTextColor={GIVER_CASUAL_NAVY}
-            onPress={() => {
-              serviceTypeKorean === "방문"
-                ? navigate("set-visiting-service-day-screen", {
-                    // TODO - 여러개의 selected 가 넘겨질 경우 처리
-                    selectedDates,
-                    visitingId: petsitter.id,
-                    isAvailableDate:
-                      _.intersection(
-                        visitingAvailableTimes.map((item) => item.date),
-                        selectedDates,
-                      ).length !== 0,
-                  })
-                : navigate("set-creche-service-day-screen", {
-                    selectedDates,
-                    crecheId: petsitter.id,
-                    isAvailableDate:
-                      _.intersection(
-                        crecheDates.map((item) => item.startDate),
-                        selectedDates,
-                      ).length !== 0,
-                    availableDate: crecheDates.find((item) => item.startDate === selectedDates[0]),
-                  })
-            }}
-          />
-        </>
-      ) : (
-        <>
-          <Image
-            source={images.cat_with_heart}
-            style={{
-              width: 158 * 2,
-              height: 122 * 2,
-              alignSelf: "center",
-              position: "absolute",
-              top: 160,
-            }}
-          />
-          <ConditionalButton
-            label={`펫시터 서비스 등록하러 가기!`}
-            isActivated={true}
-            style={{
-              alignSelf: "center",
-              position: "absolute",
-              bottom: BOTTOM_TAB_BAR_HEIGHT,
-              backgroundColor: "white",
-              borderColor: GIVER_CASUAL_NAVY,
-              borderWidth: 2,
-            }}
-            labelTextColor={GIVER_CASUAL_NAVY}
-            onPress={() => {
-              navigate("cg-mypage-screen")
-            }}
-          />
-        </>
-      )}
+            {/* 수정 버튼 */}
+            <ConditionalButton
+              label={"수정"}
+              isActivated={true}
+              style={{
+                alignSelf: "center",
+                position: "absolute",
+                bottom: BOTTOM_TAB_BAR_HEIGHT + 16,
+                backgroundColor: "white",
+                borderColor: GIVER_CASUAL_NAVY,
+                borderWidth: 2,
+              }}
+              labelTextColor={GIVER_CASUAL_NAVY}
+              onPress={() => {
+                serviceTypeKorean === "방문"
+                  ? navigate("set-visiting-service-day-screen", {
+                      // TODO - 여러개의 selected 가 넘겨질 경우 처리
+                      selectedDates,
+                      visitingId: petsitter.id,
+                      isAvailableDate:
+                        _.intersection(
+                          visitingAvailableTimes.map((item) => item.date),
+                          selectedDates,
+                        ).length !== 0,
+                    })
+                  : navigate("set-creche-service-day-screen", {
+                      selectedDates,
+                      crecheId: petsitter.id,
+                      isAvailableDate:
+                        _.intersection(
+                          crecheDates.map((item) => item.startDate),
+                          selectedDates,
+                        ).length !== 0,
+                      availableDate: crecheDates.find(
+                        (item) => item.startDate === selectedDates[0],
+                      ),
+                    })
+              }}
+            />
+          </>
+        ) : (
+          <>
+            <Image
+              source={images.cat_with_heart}
+              style={{
+                width: 158 * 2,
+                height: 122 * 2,
+                alignSelf: "center",
+                position: "absolute",
+                top: 160,
+              }}
+            />
+            <ConditionalButton
+              label={`펫시터 서비스 등록하러 가기!`}
+              isActivated={true}
+              style={{
+                alignSelf: "center",
+                position: "absolute",
+                bottom: BOTTOM_TAB_BAR_HEIGHT,
+                backgroundColor: "white",
+                borderColor: GIVER_CASUAL_NAVY,
+                borderWidth: 2,
+              }}
+              labelTextColor={GIVER_CASUAL_NAVY}
+              onPress={() => {
+                navigate("cg-mypage-screen")
+              }}
+            />
+          </>
+        )}
+      </ScrollView>
     </Screen>
   )
 })
