6fd23256d129ae54e56ca890cdc926e428e10b52
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2023-12-05 00:31:42 +0900

    MVP-60: payment-screen.controller.ts 생성 후 UI 와 관련 없는 로직 모두 분리함.

--- app/screens/_CLIENT/pay-stack/payment/payment-screen.tsx
+++ app/screens/_CLIENT/pay-stack/payment/payment-screen.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from "react"
+import React, { FC, useState } from "react"
 import { StyleSheet, View, Image, Pressable, TouchableOpacity } from "react-native"
 import { images } from "#images"
 import { observer } from "mobx-react-lite"
@@ -19,15 +19,10 @@ import {
 import { ScrollView } from "react-native-gesture-handler"
 import { BODY, GIVER_CASUAL_NAVY, MIDDLE_LINE } from "#theme"
 import IMP, { IMPData, IMPConst } from "iamport-react-native"
-import {
-  FeeResponse,
-  calculateCrecheBooking,
-  calculateVisitingBooking,
-} from "../../../../services/axios/payment-calculate"
 import { useStores } from "#models"
 import { price as priceFormatter } from "../../../../utils/format"
 import { alertModal } from "../../../../utils/alert-modal"
-import { createCrecheBooking, createPayment, createVisitingBooking } from "#axios"
+import { useCalculator, createBooking } from "./payment-screen.controller"
 
 export interface PaymentParams {
   params: IMPData.PaymentData
@@ -54,7 +49,6 @@ export const PaymentScreen: FC<StackScreenProps<NavigatorParamList, "payment-scr
     const { key, service, selectedPetIds, selectedTime, bookingRequest, destination } = route.params
     console.log("selectedTime 3", selectedTime)
     console.log("bookingRequest", bookingRequest)
-
     const {
       userStore: { userDetail },
     } = useStores()
@@ -69,7 +63,6 @@ export const PaymentScreen: FC<StackScreenProps<NavigatorParamList, "payment-scr
     const [cardQuota, setCardQuota] = useState(0)
     const [merchantUid, setMerchantUid] = useState(`mid_${new Date().getTime()}`)
     const [name, setName] = useState("아임포트 결제데이터분석")
-    const [amount, setAmount] = useState<FeeResponse>(null)
     const [buyerName, setBuyerName] = useState("홍길동")
     const [buyerTel, setBuyerTel] = useState("01012341234")
     const [buyerEmail, setBuyerEmail] = useState("example@example.com")
@@ -81,126 +74,7 @@ export const PaymentScreen: FC<StackScreenProps<NavigatorParamList, "payment-scr
     const is신용체크카드 = selectedTool === "신용/체크카드"
 
     // totalFee 계산
-    useEffect(() => {
-      const idProp = key === "visiting" ? "visitingId" : "crecheId"
-      const startProp = key === "visiting" ? "startTime" : "startDate"
-      const endProp = key === "visiting" ? "endTime" : "endDate"
-      const req = {
-        petIds: selectedPetIds,
-        [startProp]: selectedTime.start,
-        [endProp]: selectedTime.end,
-        [idProp]: service[key].id,
-      }
-      const calculator = key === "visiting" ? calculateVisitingBooking : calculateCrecheBooking
-      //@ts-ignore
-      calculator(req).then((res) =>
-        setAmount({
-          subTotalFee: res?.subTotalFee,
-          totalFee: res?.totalFee,
-        }),
-      )
-    }, [key, selectedPetIds, selectedTime.end, selectedTime.start, service])
-
-    // //* 결제 정보에 따른 data update 및 결제스크린 이동
-    // const onPressPay = () => {
-    //   const data: PaymentParams = {
-    //     params: {
-    //       pg,
-    //       pay_method: method,
-    //       currency: undefined,
-    //       notice_url: undefined,
-    //       display: undefined,
-    //       merchant_uid: merchantUid,
-    //       name,
-    //       amount,
-    //       app_scheme: "exampleforrn",
-    //       tax_free: undefined,
-    //       buyer_name: buyerName,
-    //       buyer_tel: buyerTel,
-    //       buyer_email: buyerEmail,
-    //       buyer_addr: undefined,
-    //       buyer_postcode: undefined,
-    //       custom_data: undefined,
-    //       vbank_due: undefined,
-    //       digital: undefined,
-    //       language: undefined,
-    //       biz_num: undefined,
-    //       customer_uid: undefined,
-    //       naverPopupMode: undefined,
-    //       naverUseCfm: undefined,
-    //       naverProducts: undefined,
-    //       m_redirect_url: IMPConst.M_REDIRECT_URL,
-    //       niceMobileV2: true,
-    //       escrow,
-    //     },
-    //     tierCode,
-
-    //     //? 예약 생성 api를 위한 값들
-    //     visitingId: service[key].id,
-    //     userId: userDetail?.id,
-    //     request: requests?.request,
-    //     // services: services,
-    //     destination: userDetail?.address,
-    //     // selectedDate: selectedDate,
-    //     startTime: selectedTime.start,
-    //     endTime: selectedTime.end,
-    //     petIds: selectedPetIds,
-    //     petToolsLocInfo: requests?.petToolsLocInfo,
-    //     avoidFoodInfo: requests?.avoidFoodInfo,
-    //     bondingTipsInfo: requests?.bondingTipsInfo,
-    //   }
-
-    //   // 신용카드의 경우, 할부기한 추가
-    //   if (method === "card" && cardQuota !== 0) {
-    //     data.params.display = {
-    //       card_quota: cardQuota === 1 ? [] : [cardQuota],
-    //     }
-    //   }
-
-    //   /*     // 가상계좌의 경우, 입금기한 추가
-    //   if (method === "vbank" && vbankDue) {
-    //     data.params.vbank_due = vbankDue
-    //   }
-
-    //   // 다날 && 가상계좌의 경우, 사업자 등록번호 10자리 추가
-    //   if (method === "vbank" && pg === "danal_tpay") {
-    //     data.params.biz_num = bizNum
-    //   }
-
-    //   // 휴대폰 소액결제의 경우, 실물 컨텐츠 여부 추가
-    //   if (method === "phone") {
-    //     data.params.digital = digital
-    //   }
-
-    //   // 정기결제의 경우, customer_uid 추가
-    //   if (pg === "kcp_billing") {
-    //     data.params.customer_uid = `cuid_${new Date().getTime()}`
-    //   } */
-
-    //   if (pg === "naverpay") {
-    //     const today = new Date()
-    //     const oneMonthLater = new Date(today.setMonth(today.getMonth() + 1))
-    //     const dd = String(oneMonthLater.getDate()).padStart(2, "0")
-    //     const mm = String(oneMonthLater.getMonth() + 1).padStart(2, "0") // January is 0!
-    //     const yyyy = oneMonthLater.getFullYear()
-
-    //     data.params.naverPopupMode = false
-    //     data.params.naverUseCfm = `${yyyy}${mm}${dd}`
-    //     data.params.naverProducts = [
-    //       {
-    //         categoryType: "BOOK",
-    //         categoryId: "GENERAL",
-    //         uid: "107922211",
-    //         name: "한국사",
-    //         payReferrer: "NAVER_BOOK",
-    //         count: 10,
-    //       },
-    //     ]
-    //   }
-
-    //   console.log("Payment data >>>", data)
-    //   navigate("test-iamport-payment-screen", data)
-    // }
+    const amount = useCalculator({ key, selectedPetIds, selectedTime, service })
 
     /**
      * [개발중 🏗️]
@@ -221,64 +95,22 @@ export const PaymentScreen: FC<StackScreenProps<NavigatorParamList, "payment-scr
         )
       }
 
-      //TODO: 1. 예약 결제 가격 계산
-      //TODO:     /api/v1/payment/creche-booking/calculate 를 통해 결제될 총 금액을 계산합니다.
-      //TODO: 2. 결제 사전 등록
-      //TODO:   /api/v1/payment/invoice 를 통해 예상 결제 금액을 서버에 전달합니다.
-      //TODO: 3. 프론트엔드에서 포트원을 띄워 해당 정보로 결제를 진행함.
-      //TODO: 4. 결제 사후 정보 확인
-      //TODO:   /api/v1/payment/confirm 포트원 결제 이후에 발행된 imp_uid와 merchant_uid를 통해 결제가 제대로 이루어졌는지 확인합니다.
-      //TODO: 5. 결제 정보 DB 저장
-      //TODO:   POST /api/v1/payment 를 통해 정상적으로 처리된 결제 내용을 DB에 저장합니다.
-      //TODO: 6.예약 완료
-      //TODO:   POST /api/v1/booking/creche 를 통해 결제 정보와 같이 예약을 생성합니다.
-
-      // 5.
-      createPayment({
-        imp_uid: "111",
-        merchant_uid: "111",
-        imp_success: true,
-        isRefunded: false,
-        totalFee: amount.totalFee,
-      }).then((res) => {
-        if (res.isSuccess) {
-          // 6.
-          const creator = key === "visiting" ? createVisitingBooking : createCrecheBooking
-          const idProp = key === "visiting" ? "visitingId" : "crecheId"
-          const startProp = key === "visiting" ? "startTime" : "startDate"
-          const endProp = key === "visiting" ? "endTime" : "endDate"
-          const req = {
-            [idProp]: service[key].id,
-            userId: userDetail.id,
-            [startProp]: selectedTime.start,
-            [endProp]: selectedTime.end,
-            petIds: selectedPetIds,
-            paymentId: res.paymentId,
-            request: bookingRequest.request,
-            avoidFoodInfo: bookingRequest.avoidFoodInfo,
-            bondingTipsInfo: bookingRequest.bondingTipsInfo,
-          }
-          if (key === "visiting") {
-            req.petToolsLocInfo = bookingRequest.petToolsLocInfo
-            req.destination = destination
-          }
-          console.log("req 🔷", req)
-          //@ts-ignore
-          creator(req).then((res) => {
-            if (res.isSuccess) {
-              alertModal(
-                "결제기능 개발중 🏗️",
-                "실제 결제는 이루어지지 않았으며, 예약 객체가 성공적으로 생성되었습니다.",
-              )
-              setSuccessModalVisible(true)
-            } else {
-              alertModal(
-                "예약 객체 생성 실패",
-                "알 수 없는 이유로, 예약 객체 생성에 실패하였습니다. 잠시 후, 다시 시도해주세요.",
-              )
-            }
-          })
-        }
+      createBooking({
+        key,
+        userId: userDetail.id,
+        selectedTime,
+        selectedPetIds,
+        bookingRequest,
+        destination,
+        service,
+        setSuccessModalVisible,
+        paymentData: {
+          imp_uid: "111",
+          merchant_uid: "111",
+          imp_success: true,
+          isRefunded: false,
+          totalFee: amount.totalFee,
+        },
       })
     }
 
