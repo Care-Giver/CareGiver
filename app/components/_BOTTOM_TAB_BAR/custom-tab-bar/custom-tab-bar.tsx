825121f7594be74c0f5da6c62e7a80b807b10323
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2023-12-17 07:18:21 +0900

    [QA] MVP-62: 채팅 탭, 애플 계정 유저일 경우 플로우 진행을 차단 함.

--- app/components/_BOTTOM_TAB_BAR/custom-tab-bar/custom-tab-bar.tsx
+++ app/components/_BOTTOM_TAB_BAR/custom-tab-bar/custom-tab-bar.tsx
@@ -37,6 +37,7 @@ export const CustomTabBar = observer(function CustomTabBar(props: CustomTabBarPr
 
   const {
     uiStore: { showingBottomTab },
+    userStore: { userAuth },
   } = useStores()
 
   const animatedBottomTabBarHeight = useRef(new Animated.Value(BOTTOM_TAB_BAR_HEIGHT)).current
@@ -131,10 +132,16 @@ export const CustomTabBar = observer(function CustomTabBar(props: CustomTabBarPr
         const onPress = () => {
           // 아직 미구현된 탭들 핸들링 - 채팅(CG, CL) 탭, 통계(CG) 탭
           switch (label) {
-            // //  채팅(CG, CL) 탭
-            // case tabLabel.chatting:
-            //   alertModal("MVP", "채팅 기능은 아직 개발중입니다 🦮")
-            //   return
+            //  채팅(CG, CL) 탭
+            case tabLabel.chatting:
+              if (userAuth.provider === "apple") {
+                alertModal(
+                  "개발중 🏗️",
+                  "아쉽게도 현재, 애플계정으로 회원가입 한 유저는 이메일 정보를 받아올 수 없으므로 채팅 기능을 사용할 수 없습니다.",
+                )
+                return
+              }
+              break
 
             // // 예약관리(CG) 탭
             // case tabLabel.manage_booking:
