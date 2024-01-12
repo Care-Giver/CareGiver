500248c931adf34a2ee057a523c952fd112a0689
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2024-01-13 11:54:53 +1300

    SDK 업데이트 이후, ios "No bundle URL present" 에러 해결: ios jsBundleURLForBundleRoot 경로 수정, main.jsbundle path 매핑 삭제. # 빌드에러 발생시, 다음 명령어 순서대로 실행할 것. - rm -rf ios/build/; kill $(lsof -t -i:8081); react-native run-ios - rm -rf ~/Library/Developer/Xcode/DerivedData - yarn c - cd ios && pod install && cd .. - yarn ios

--- ios/CareGiver/AppDelegate.mm
+++ ios/CareGiver/AppDelegate.mm
@@ -31,7 +31,7 @@
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@".expo/.virtual-metro-entry"];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
