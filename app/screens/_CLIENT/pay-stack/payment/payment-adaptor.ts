89bc324b0f1757d5a7d21a08e841321d26518743
minSsan <msp1125@hanyang.ac.kr>
2024-01-20 18:03:21 +0900

    MVP-68 Feat: payment request body 생성 Adpator 클래스 생성

---
+++ app/screens/_CLIENT/pay-stack/payment/payment-adaptor.ts
@@ -0,0 +1,60 @@
+import { ServiceType } from "#models"
+import { SelectedTime } from "#navigators"
+import { VisitingCreche } from "../../search-stack/search-result-screen/search-result-screen"
+
+export type RawPayment = {
+  key: ServiceType
+  selectedPetIds: number[]
+  selectedTime: SelectedTime
+  service: VisitingCreche
+}
+
+export class PaymentAdaptor {
+  private value: RawPayment
+
+  constructor(obj: RawPayment) {
+    this.value = obj
+  }
+
+  get idProp() {
+    switch (this.value.key) {
+      case "visiting":
+        return "visitingId"
+      case "creche":
+        return "crecheId"
+      default:
+        throw new Error("잘못된 service type 입니다.")
+    }
+  }
+
+  get startProp() {
+    switch (this.value.key) {
+      case "visiting":
+        return "startTime"
+      case "creche":
+        return "startDate"
+      default:
+        throw new Error(`잘못된 service type 입니다. serviceType: ${this.value.key}`)
+    }
+  }
+
+  get endProp() {
+    switch (this.value.key) {
+      case "visiting":
+        return "endTime"
+      case "creche":
+        return "endDate"
+      default:
+        throw new Error(`잘못된 service type 입니다. serviceType: ${this.value.key}`)
+    }
+  }
+
+  adapt() {
+    return {
+      [this.idProp]: this.value.service[this.value.key].id,
+      [this.startProp]: this.value.selectedTime.start,
+      [this.endProp]: this.value.selectedTime.end,
+      petIds: this.value.selectedPetIds,
+    }
+  }
+}
