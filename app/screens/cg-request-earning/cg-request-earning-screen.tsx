695fecd8e0eb8c298b372e59bf1011f544fa0d78
hojkim77 <rlaghwnd4534@gmail.com>
2024-01-26 00:06:54 +0900

    MVP-69 정산 요청 스크린 UI 구현

--- app/screens/cg-request-earning/cg-request-earning-screen.tsx
+++ app/screens/cg-request-earning/cg-request-earning-screen.tsx
@@ -1,11 +1,41 @@
 import React, { FC, useState } from "react"
-import { Image, ScrollView, StyleSheet, View } from "react-native"
+import { Image, Linking, Pressable, ScrollView, StyleSheet, Text, View } from "react-native"
 import { observer } from "mobx-react-lite"
 import { StackScreenProps } from "@react-navigation/stack"
 import { NavigatorParamList } from "#navigators"
-import { PreBol14, PreBol20, PreMed12, PreReg16, RowRoundedBox, Screen } from "#components"
-import { CARE_NATURAL_BLUE, GIVER_CASUAL_NAVY, HEAD_LINE, LBG } from "#theme"
+import {
+  BlueCheckbox,
+  ConditionalButton,
+  CustomModal,
+  DivisionLine,
+  PlaceHolderInputBox,
+  PopSem24,
+  PreBol14,
+  PreBol16,
+  PreBol20,
+  PreMed12,
+  PreMed16,
+  PreReg14,
+  PreReg16,
+  Row,
+  RowRoundedBox,
+  Screen,
+} from "#components"
+import {
+  BODY,
+  BOTTOM_HEIGHT,
+  CARE_NATURAL_BLUE,
+  DISABLED,
+  GIVER_CASUAL_NAVY,
+  HEAD_LINE,
+  LBG,
+  LIGHT_LINE,
+} from "#theme"
 import { images } from "#images"
+import { l } from "i18n-js"
+import { BottomSheetFlatList } from "@gorhom/bottom-sheet"
+import { 외부링크 } from "../../services/external-web-link"
+import { ScrollToBottomButton } from "stream-chat-react-native"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "#models"
 
@@ -15,27 +45,148 @@ import { images } from "#images"
 export const CgRequestEarningScreen: FC<
   StackScreenProps<NavigatorParamList, "cg-request-earning-screen">
 > = observer(function CgRequestEarningScreen() {
-  const [isOpen, setIsOpen] = useState<boolean>(false)
-  const placeholderBoxStyle = isOpen ? styles.placeholderBoxOpen : styles.placeholderBoxClosed
+  //* 정산 관련 정보
+  const [bank, setBank] = useState<string>("")
+  const [account, setAccount] = useState<string>("")
+  const [name, setName] = useState<string>("")
 
+  //* 은행 선택 버튼
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  //* 약관 동의 버튼
+  const [isCheck, setIsCheck] = useState<boolean>(false)
+  const onCheckPress = () => setIsCheck(!isCheck)
+
+  const [modalOpen, setModalOpen] = useState<boolean>(false)
+
+  /**
+   * 표출할 스크린 상태
+   * false  = 정산정보 입력 스크린
+   * true = 정산 요청  스크린
+   */
+  const [isConfirmed, setIsConfirmed] = useState<boolean>(false)
+  const onPressBottomButton = () => {
+    if (isConfirmed === false) setIsConfirmed(true)
+    if (isConfirmed === true) setModalOpen(true)
+  }
+  const placeholderBoxStyle = isOpen ? styles.placeholderBoxOpen : styles.placeholderBoxClosed
+  const openLink = (link: string) => {
+    Linking.openURL(link)
+  }
+  const banks = [
+    {
+      name: "국민은행",
+    },
+    { name: "신한은행" },
+  ]
   return (
     <Screen testID="CgRequestEarning">
-      <ScrollView scrollEnabled={!!isOpen}>
-        <PreBol20 mt={15} text="정산 받을" />
-        <PreBol20 mt={6} text="계좌를 알려주세요" />
+      <ScrollView scrollEnabled={isConfirmed}>
+        <PreBol20 mt={15} text={isConfirmed ? "다음 달 1일에" : "정산 받을"} />
+        <PreBol20 mt={6} text={isConfirmed ? "받을 금액을 확인해주세요." : "계좌를 알려주세요"} />
         <View style={styles.underBar} />
 
-        <View style={styles.notificationBox}>
-          <PreBol14 text="정산 요청 전, 잠깐!" color={GIVER_CASUAL_NAVY} mb={8} />
-          <PreMed12 text="펫시팅 요금의 경우 매달 1일에 정산하여 등록해주신 계좌로 입금해드립니다. 연휴나 공휴일에는 지급이 지연될 수 있는 점 양해 부탁드립니다." />
-        </View>
-
-        <RowRoundedBox style={placeholderBoxStyle} onPress={() => setIsOpen(!isOpen)}>
-          <PreReg16 text={"은행 선택"} color={HEAD_LINE} />
-          <Image source={!isOpen ? images.arrow_down : images.arrow_up} style={styles.image} />
-        </RowRoundedBox>
-        {isOpen && <View></View>}
+        {!isConfirmed ? (
+          <View>
+            <View style={styles.notificationBox}>
+              <PreBol14 text="정산 요청 전, 잠깐!" color={GIVER_CASUAL_NAVY} mb={8} />
+              <PreMed12 text="펫시팅 요금의 경우 매달 1일에 정산하여 등록해주신 계좌로 입금해드립니다. 연휴나 공휴일에는 지급이 지연될 수 있는 점 양해 부탁드립니다." />
+            </View>
+            <RowRoundedBox
+              style={placeholderBoxStyle}
+              preset="Pressable"
+              onPress={() => setIsOpen(!isOpen)}
+            >
+              <PreReg16 text={"은행 선택"} color={HEAD_LINE} />
+              <Image source={!isOpen ? images.arrow_down : images.arrow_up} style={styles.image} />
+            </RowRoundedBox>
+            {isOpen && (
+              <View
+                style={{
+                  height: "auto",
+                  borderColor: GIVER_CASUAL_NAVY,
+                  borderWidth: 2,
+                  borderBottomLeftRadius: 8,
+                  borderBottomRightRadius: 8,
+                }}
+              >
+                <BottomSheetFlatList
+                  data={banks}
+                  renderItem={({ item }) => (
+                    <View>
+                      <Text>{item.name}</Text>
+                    </View>
+                  )}
+                />
+              </View>
+            )}
+            <PlaceHolderInputBox
+              backgroundColor="white"
+              borderColor={LIGHT_LINE}
+              style={{ marginTop: 12 }}
+              boxHeight={48}
+              placeholderText="계좌번호를 입력해주세요."
+              text={account}
+              setText={setAccount}
+            />
+            <PlaceHolderInputBox
+              backgroundColor="white"
+              borderColor={LIGHT_LINE}
+              style={{ marginTop: 12 }}
+              boxHeight={48}
+              placeholderText="예금주명을 입력해주세요."
+              text={name}
+              setText={setName}
+            />
+            <DivisionLine mt={20} mb={20} />
+            <Row mb={12}>
+              <BlueCheckbox value={isCheck} onPress={onCheckPress}></BlueCheckbox>
+              <PreMed16 text="개인정보 수집 이용 동의(필수)" ml={8} />
+              <Pressable
+                style={{ marginLeft: "auto" }}
+                onPress={() => openLink(외부링크.개인정보_수집_이용_동의)}
+              >
+                <Image source={images.arrow_right} style={{ width: 16, height: 16 }} />
+              </Pressable>
+            </Row>
+          </View>
+        ) : (
+          <View>
+            <RowRoundedBox
+              style={styles.placeholderBoxClosed}
+              preset="Pressable"
+              onPress={() => setIsOpen(!isOpen)}
+            >
+              <PopSem24 text="25000" color={GIVER_CASUAL_NAVY} />
+              <PreBol16 text="원" />
+              <PreMed16 style={{ marginLeft: "auto" }} text="자세히" color={BODY} />
+              <Image
+                source={!isOpen ? images.arrow_down : images.arrow_up}
+                style={[styles.image, { marginLeft: 3 }]}
+              />
+            </RowRoundedBox>
+          </View>
+        )}
       </ScrollView>
+      <ConditionalButton
+        label={isConfirmed ? "정산 요청하기" : "다음"}
+        isActivated={isCheck}
+        style={{ position: "absolute", bottom: BOTTOM_HEIGHT, alignSelf: "center" }}
+        //TODO navigation추가 필요
+        onPress={onPressBottomButton}
+      />
+      <CustomModal
+        visibleState={modalOpen}
+        noBtnText="요청 목록 확인"
+        yesBtnText="홈으로 가기"
+        image={images.cat_with_heart}
+        imageWidth={154}
+        imageHeight={120}
+        title="입금은 다음 달 1일에 돼요!"
+        subtitle="정산 요청이 정상적으로 완료되었어요."
+        handleNoPress={() => setModalOpen(false)}
+        handleYesPress={() => setModalOpen(false)}
+      />
     </Screen>
   )
 })
@@ -60,9 +211,11 @@ const styles = StyleSheet.create({
     marginLeft: "auto",
   },
   placeholderBoxClosed: {
+    marginTop: 28,
     paddingHorizontal: 16,
   },
   placeholderBoxOpen: {
+    marginTop: 28,
     paddingHorizontal: 16,
     borderBottomWidth: 0,
     borderBottomLeftRadius: 0,
