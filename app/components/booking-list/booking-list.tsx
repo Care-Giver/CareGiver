6912a144e0f83a68351324f1a715434c74489a98
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2024-01-29 01:08:10 +1300

    MVP-70: "예약 관리" 에서 쓰였던 모든 react-native-calendars 관련 코드 삭제

--- app/components/booking-list/booking-list.tsx
+++ app/components/booking-list/booking-list.tsx
@@ -1,52 +1,105 @@
-import React, { forwardRef, useCallback, useEffect, useMemo, useRef, useState } from "react"
-import { StyleProp, ViewStyle, View, Image, Pressable } from "react-native"
-import { observer, useLocalObservable, useObserver } from "mobx-react-lite"
-import { images } from "#images"
-import { CalendarProvider, AgendaList, ExpandableCalendar } from "react-native-calendars"
-import { GIVER_CASUAL_NAVY } from "#theme"
+import React, { useMemo, useRef } from "react"
+import { StyleProp, ViewStyle, View, FlatList, Image } from "react-native"
+import { observer } from "mobx-react-lite"
+import { BOTTOM_HEIGHT, BOTTOM_TAB_NAVIGATOR, GIVER_CASUAL_NAVY } from "#theme"
 import { BookingInfoCard } from "../booking-info-card/booking-info-card"
 import { PreBol16, PreMed18, PreReg12, PreReg14 } from "../_BASIC/custom-texts/custom-texts"
-import { ConfirmedBooking } from "#axios"
+import { CgBooking, ConfirmedBooking } from "#axios"
 import { DateData, DayState } from "react-native-calendars/src/types"
-import { UpdateSources } from "react-native-calendars/src/expandableCalendar/commons"
-import { autorun, observable } from "mobx"
+import { BOTTOM_TAB_BAR_HEIGHT } from "../_BOTTOM_TAB_BAR/custom-tab-bar/custom-tab-bar"
+import { images } from "#images"
 
 export interface BookingListProps {
   /**
    * 추가적인 padding, margin 을 줌으로써, 위치를 조정할 수 있습니다.
    */
+  style?: StyleProp<ViewStyle>
 
   /**
    * 예약 객체 배열
    */
-  sections: any[]
-
-  style?: StyleProp<ViewStyle>
+  bookings: CgBooking[]
 }
 
+export const BookingList = observer(function BookingList(props: BookingListProps) {
+  const { bookings = [], style } = props
+
+  //* 선택된 날짜
+  const selected = useRef<string>("")
+  //const [selected, setSelected] = useState("")
+
+  const onDayPress = ({ date }) => {
+    console.log("PRESSED 🔷", date)
+    selected.current = date.dateString
+    console.log("SELECTED 🔷", selected)
+    //setSelected(date.dateString)
+  }
+
+  console.log("bookings", bookings)
+
+  const allStyles = Object.assign({}, { flex: 1, width: "100%", height: "100%" }, style)
+
+  return (
+    <FlatList
+      style={allStyles}
+      contentContainerStyle={{ paddingBottom: BOTTOM_TAB_BAR_HEIGHT }}
+      data={bookings}
+      renderItem={({ item, index }) => {
+        if (item === null || item === undefined) {
+          //TODO: 빈 날짜일 경우 UI 처리
+          console.log("item is null or undefined:", `${JSON.stringify(item)}`)
+          return <PreBol16 text={JSON.stringify(item)} />
+        }
+        const visOrCre = item?.crecheBookingId ? "위탁" : item?.visitingBookingId ? "방문" : "ERR"
+        const dateOrTime =
+          visOrCre === "방문" ? item.startTime : visOrCre === "위탁" ? item.startDate : null
+        const startProp = visOrCre === "방문" ? "startTime" : "startDate"
+        const endProp = visOrCre === "방문" ? "endTime" : "endDate"
+
+        return (
+          <View
+            style={{ display: "flex", flexDirection: "row" }}
+            key={(item?.crecheBookingId || item?.visitingBookingId) + 100 * index}
+          >
+            {/* 좌측 시간대 표기 UI */}
+            {/* <View
+                style={{
+                  marginVertical: 16,
+                  marginRight: 8,
+                  paddingRight: 5,
+                  justifyContent: "space-between",
+                  borderRightWidth: 2,
+                  borderColor: "#F8F8FA",
+                }}
+              >
+                <PreBol16 color={GIVER_CASUAL_NAVY}>{item[startProp].substring(11, 16)}</PreBol16>
+                <PreBol16 color={GIVER_CASUAL_NAVY}>{item[endProp].substring(11, 16)}</PreBol16>
+              </View> */}
+
+            {/* 예약 요약 카드 */}
+            <BookingInfoCard style={{ marginVertical: 8 }} booking={item} visOrCre={visOrCre} />
+          </View>
+        )
+      }}
+      ListEmptyComponent={() => (
+        <View style={{ alignSelf: "center", alignItems: "center" }}>
+          <Image source={images.dog_question} style={{ width: 179, height: 192 }} />
+          <PreMed18 text={`진행 중 이거나 완료한 예약이 없습니다`} />
+        </View>
+      )}
+    />
+  )
+})
+
+// TODO: 월 일 달력 UI 구현시 CustomDayComponent 를 일 UI 구현때 사용할 것
 interface CustomDayComponentProps {
   date: string & DateData
   state: DayState
   selected: any
 }
-export const CustomDayComponent = observer(
-  function BookingList(props: CustomDayComponentProps) {
+const CustomDayComponent = observer(
+  function CustomDayComponent(props: CustomDayComponentProps) {
     const { date, state, selected } = props
-
-    //* test
-    // const myObservableRef = observable({
-    //   value: selected.current,
-    // })
-
-    // useEffect(() => {
-    //   const dispose = autorun(() => {
-    //     console.log("Value changed:", myObservableRef.value)
-    //   })
-
-    //   return () => {
-    //     dispose()
-    //   }
-    // }, [myObservableRef])
 
     const { translateWeekText, textBgBdSelectior, textColorSelectior } = useMemo(() => {
       const res = {
@@ -71,18 +124,6 @@ export const CustomDayComponent = observer(
 
       return res
     }, [date, state, selected])
-
-    // const textBgBdSelectior = () => {
-    //   if (date.dateString === selected) return GIVER_CASUAL_NAVY
-    //   return "#FFFFFF"
-    // }
-    // const textColorSelectior = () => {
-    //   if (date.dateString === selected) return GIVER_CASUAL_NAVY
-    //   return "#FFFFFF"
-    // }
-    // const translateWeekText = () => {
-    //   return week[new Date(date.timestamp).getDay()]
-    // }
 
     return (
       <View
@@ -122,191 +163,3 @@ export const CustomDayComponent = observer(
   },
   { forwardRef: true },
 )
-
-export const BookingList = observer(function BookingList(props: BookingListProps) {
-  const { sections } = props
-
-  //* 선택된 날짜
-  const selected = useRef<string>("")
-  //const [selected, setSelected] = useState("")
-
-  const onDayPress = ({ date }) => {
-    console.log("PRESSED 🔷", date)
-
-    selected.current = date.dateString
-    console.log("SELECTED 🔷", selected)
-
-    //setSelected(date.dateString)
-  }
-
-  //* AgendaList컴포넌트에서 렌더링을 위한 부분
-  const renderItem = useCallback(
-    ({ item }) => {
-      console.log("renderItem")
-      if (item === null || item === undefined) {
-        //TODO: 빈 날짜일 경우 UI 처리
-        console.log("item is null or undefined:", `${JSON.stringify(item)}`)
-        return <PreBol16 text={JSON.stringify(item)} />
-      }
-      const visOrCre = item?.crecheBookingId ? "위탁" : item?.visitingBookingId ? "방문" : "ERR"
-      const dateOrTime =
-        visOrCre === "방문" ? item.startTime : visOrCre === "위탁" ? item.startDate : null
-      const startProp = visOrCre === "방문" ? "startTime" : "startDate"
-      const endProp = visOrCre === "방문" ? "endTime" : "endDate"
-
-      // 선택된 날짜에 예약 객체 존재
-      if (dateOrTime.substring(0, 10) === selected.current) {
-        return (
-          <View style={{ display: "flex", flexDirection: "row" }}>
-            <View
-              style={{
-                marginVertical: 16,
-                marginRight: 8,
-                paddingRight: 5,
-                justifyContent: "space-between",
-                borderRightWidth: 2,
-                borderColor: "#F8F8FA",
-              }}
-            >
-              <PreBol16 color={GIVER_CASUAL_NAVY}>{item[startProp].substring(11, 16)}</PreBol16>
-              <PreBol16 color={GIVER_CASUAL_NAVY}>{item[endProp].substring(11, 16)}</PreBol16>
-            </View>
-            <BookingInfoCard
-              style={{ marginVertical: 8, marginHorizontal: 6 }}
-              booking={item}
-              visOrCre={visOrCre}
-            />
-          </View>
-        )
-      }
-      // 선택된 날짜에 예약 객체 존재하지 않음
-      else {
-        // TODO - FIXME: 한 개만 렌더링.
-        return (
-          <View style={{ alignSelf: "center", alignItems: "center" }}>
-            <Image source={images.dog_question} style={{ width: 179, height: 192 }} />
-            <PreMed18 text={`해당 날짜에는 예약이 없습니다`} />
-          </View>
-        )
-      }
-    },
-    [selected],
-  )
-
-  return (
-    <View style={{ flex: 1, marginTop: -20 }}>
-      <CalendarProvider numberOfDays={5} date={new Date().toISOString().split("T")[0]}>
-        <View style={{ display: "flex", alignItems: "flex-end" }}>
-          <ExpandableCalendar
-            // // Initially visible month. Default = now
-            // initialDate={"2024-01-16"}
-            // // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-            // minDate={"2024-01-15"}
-            // // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-            // maxDate={"2024-01-21"}
-            monthFormat={"MMMM"}
-            disablePan={true}
-            theme={{
-              monthTextColor: GIVER_CASUAL_NAVY,
-              textMonthFontSize: 20,
-              textMonthFontWeight: "bold",
-            }}
-            dayComponent={({ date, state }) => (
-              <Pressable onPress={(e) => onDayPress({ date })}>
-                {/**
-                 //* observer로 감싼 CustomDayComponent가 리렌더링되지 않음.
-                 //* selected는 분명 바뀜
-                 //? BookingList자체가 리렌더링 되어야 CustomDayComponent가 리렌더링 될지 기회가 생기는 것 아닌가?
-                 */}
-                <CustomDayComponent date={date} state={state} selected={selected} />
-              </Pressable>
-            )}
-            //? 이걸 선언하지 않으면 CustomDayComponent가 정상적인 렌더링이 안됨,,,
-            //onDayPress={(date) => onDayPress({ date })}
-            headerStyle={{
-              marginTop: 25, // default headertitle(week)을 지우기 위함
-            }}
-            calendarStyle={{
-              paddingBottom: 8,
-              borderStyle: "solid",
-              borderBottomWidth: 2,
-              borderBottomColor: "#F8F8FA",
-            }}
-            style={{
-              alignSelf: "center",
-              width: "109%",
-            }}
-            renderArrow={(direction) =>
-              direction === "left" ? (
-                <Image
-                  source={images.arrow_left_navy}
-                  style={{ width: 18, height: 18, marginLeft: 90 }}
-                />
-              ) : (
-                <Image
-                  source={images.arrow_right_navy}
-                  style={{ width: 18, height: 18, marginRight: 90 }}
-                />
-              )
-            }
-          />
-
-          {!!sections && sections.length > 0 && (
-            <AgendaList
-              sectionStyle={{ display: "none" }}
-              style={{
-                marginTop: 32,
-                marginHorizontal: -6,
-              }}
-              sections={sections}
-              renderItem={renderItem}
-              scrollToNextEvent={true}
-            />
-          )}
-        </View>
-      </CalendarProvider>
-
-      {/**test components */}
-      {/*<CalendarProvider date={"2023-06-26"} showTodayButton>
-        <CalendarHeader
-          renderArrow={(direction) =>
-            direction === "left" ? (
-              <Image
-                source={images.arrow_left_navy}
-                style={{ marginLeft: 40, width: 18, height: 18 }}
-              />
-            ) : (
-              <Image
-                source={images.arrow_right_navy}
-                style={{ marginRight: 40, width: 18, height: 18 }}
-              />
-            )
-          }
-          onPressArrowLeft={onPressArrowLeft}
-          onPressArrowRight={onPressArrowRight}
-          style={{ backgroundColor: "#FFFFFF" }}
-          hideDayNames
-          customHeaderTitle={
-            // headerMonth
-            <View>
-              <Text>{month + "월"}</Text>
-            </View>
-          }
-        />
-        <WeekCalendar
-          current={currentDate}
-          month={month}
-          allowShadow={false}
-          style={{ backgroundColor: "#FFFFFF", marginVertical: 0 }}
-          hideDayNames
-          onMonthChange={onMonthChange}
-          numberOfDays={5}
-          staticHeader={true}
-          dayComponent={CustomDayComponent}
-        />
-
-        <AgendaList sections={sections} renderItem={renderItem} />
-        </CalendarProvider>*/}
-    </View>
-  )
-})
--- app/screens/_CARE_GIVER/cg-manage-booking/cg-manage-booking-screen.tsx
+++ app/screens/_CARE_GIVER/cg-manage-booking/cg-manage-booking-screen.tsx
@@ -16,7 +16,7 @@ import { useShowBottomTab } from "../../../utils/hooks"
 import { getAllBookings, getConfirmedBookings } from "#axios"
 import { images } from "#images"
 import { Image, View } from "react-native"
-import { BODY } from "#theme"
+import { BODY, BOTTOM_HEIGHT, DEVICE_SCREEN_HEIGHT } from "#theme"
 import { useQuery } from "@tanstack/react-query"
 import { useStores } from "#models"
 
@@ -60,7 +60,7 @@ export const CgManageBookingScreen: FC<
 
   //console.log("cgManageBookingScreen >>> ", status, data, error, isFetching)
   //console.log("cgManageBookingScreen allBookings >>> ", allBookings)
-  //console.log("cgManageBookingScreen confirmedBookings >>> ", confirmedBookings)
+  console.log("cgManageBookingScreen confirmedBookings >>> ", confirmedBookings)
 
   useEffect(() => {
     if (!isFetching) {
@@ -78,33 +78,24 @@ export const CgManageBookingScreen: FC<
 
       <View
         style={{
-          width: "90%",
-          height: 200,
+          width: "100%",
+          height: "auto",
           backgroundColor: "rgba(255, 0, 0, 0.2);",
-          position: "absolute",
           alignSelf: "center",
-          bottom: 80,
-          zIndex: 5,
-          //
           padding: 16,
+          marginTop: 10,
         }}
       >
-        <PreBol14
-          text={`⬆️ 이곳은 '진행예정, '진행중' 그리고 '완료된'\n예약이 표출되는 영역입니다.`}
-          mv={12}
-        />
+        <PreBol14 text={`이곳은 추후에 달력 UI 가 표출되는 영역입니다.`} />
         <PreBol14 text="현재 개발중인 영역입니다. 🏗️" />
         <PreReg14
+          mt={10}
           text={`'모든' 예약 개수: ${allBookings.length}\n'응답을 기다리는' 예약 개수: ${waitingBookings.length}\n'거절'한 예약 개수: ${rejectedBookings.length}\n'수락'하거나 '진행중'인 예약 개수: ${confirmedBookings.length}`}
         />
       </View>
 
-      <BookingList
-        sections={confirmedBookings.map((item, idx) => {
-          const newData = { title: String(idx), data: [item] }
-          return newData
-        })}
-      />
+      {/* 모든 예약 목록 */}
+      <BookingList bookings={confirmedBookings} />
 
       {/* // TODO: 날짜별로 확정된 예약 필터링 해야 함 */}
       {/* <View style={{ alignItems: "center", top: "25%" }}>
