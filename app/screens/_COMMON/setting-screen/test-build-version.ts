ba7c5e283e9bc66d62bdbf933b55ce22cbd37902
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2024-01-29 22:32:52 +1300

    Update version (T.24.01.29.a) versionCode 9➡️10

--- app/screens/_COMMON/setting-screen/test-build-version.ts
+++ app/screens/_COMMON/setting-screen/test-build-version.ts
@@ -1,5 +1,5 @@
-const VERSION = "24.01.22.포트원_심사_제출용"
+const VERSION = "T.24.01.29.a"
 
-const TEST_BUILD_VERSION = `테스트빌드 ${VERSION}`
+const TEST_BUILD_VERSION = `Version ${VERSION}`
 
 export default TEST_BUILD_VERSION
