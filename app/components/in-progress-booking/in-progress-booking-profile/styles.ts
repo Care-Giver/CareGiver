9be560f76cbdfd2ffad9f9fdac227479ccb273fd
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2024-02-04 16:14:35 +1300

    Fix(in-progress-booking, past-booking): 좁은 스크린 S22 에서 UI 깨짐 해결

--- app/components/in-progress-booking/in-progress-booking-profile/styles.ts
+++ app/components/in-progress-booking/in-progress-booking-profile/styles.ts
@@ -1,18 +1,23 @@
+import { WIDTH } from "#theme"
 import { StyleSheet } from "react-native"
+
+const PROFILE_IMG_WIDTH = 142 * WIDTH
 
 export const styles = StyleSheet.create({
   //   root: {
   //     flexDirection: "row",
   //   },
   profileImg: {
-    width: 142,
+    width: PROFILE_IMG_WIDTH,
     height: 108,
     borderRadius: 9,
     backgroundColor: "black",
   },
+  infoRoot: {
+    marginLeft: 18 * WIDTH,
+    flex: 1,
+  },
   infoBox: {
-    // height: 63,
-    // flexDirection: "column",
-    // justifyContent: "space-between",
+    marginTop: 10,
   },
 })
--- app/components/past-booking/styles.ts
+++ app/components/past-booking/styles.ts
@@ -1,15 +1,13 @@
-import { GIVER_CASUAL_NAVY, LIGHT_LINE, palette } from "../../theme"
+import { GIVER_CASUAL_NAVY, LIGHT_LINE, WIDTH, palette } from "../../theme"
 import { StyleSheet } from "react-native"
 
 export const styles = StyleSheet.create({
   root: {
+    width: "100%",
     paddingHorizontal: 16,
     paddingVertical: 16,
 
     flexDirection: "row",
-    // alignItems: "center",
-    // justifyContent: "space-between",
-
     borderRadius: 8,
     borderColor: LIGHT_LINE,
     borderWidth: 1,
@@ -29,9 +27,10 @@ export const styles = StyleSheet.create({
   },
 
   profileImg: {
-    width: 142,
+    // width: 12 * WIDTH,
+    // width: "auto",
     height: 108,
-
+    flex: 1,
     padding: 8,
   },
 
@@ -41,12 +40,11 @@ export const styles = StyleSheet.create({
   },
 
   bookingInfo: {
-    // width: 164,
-    flex: 1,
-    marginLeft: 16,
+    width: "auto",
+    flex: 2,
+    marginLeft: 16 * WIDTH,
     flexDirection: "column",
     justifyContent: "space-between",
-
     paddingVertical: 7.5,
   },
 
