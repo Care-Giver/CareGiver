2a88471e69550d7ea2f9e335b0a557da958a24d2
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2024-02-04 23:31:34 +1300

    Update(past-booking): "다시 예약하기" / "후기 작성하기" 버튼 숨김 - TODO: "후기 작성" 기능 테스트 완료한 뒤 복구하기

--- app/components/past-booking/past-booking.tsx
+++ app/components/past-booking/past-booking.tsx
@@ -217,7 +217,9 @@ export const PastBooking = (props: PastBookingProps) => {
         </Row>
 
         {/* //* 다시 예약하기 | 후기 작성하기 */}
-        <Row style={{}}>
+        {/* // TODO: "후기 작성" 기능 테스트 완료 후 다시 표출하기 */}
+        {/* // FIXME: "다시 예약하기"는 유저 플로우가 어떻게 되는 건가? */}
+        <Row style={{ display: "none" }}>
           {/* //? 다시 예약하기 버튼 */}
           <Pressable onPress={handleAgainPress}>
             <PreMed14 text={"다시 예약하기"} color={HEAD_LINE} />
