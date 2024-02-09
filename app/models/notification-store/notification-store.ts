17a5361cf6662dcb0ac98a7112228297153a7f55
minSsan <msp1125@hanyang.ac.kr>
2024-02-09 19:34:12 +0900

    MVP-81 feat: NotificationStoreModel 수정

--- app/models/notification-store/notification-store.ts
+++ app/models/notification-store/notification-store.ts
@@ -1,11 +1,6 @@
 import { Instance, SnapshotOut, applySnapshot, types } from "mobx-state-tree"
-import { withSetPropAction } from "../extensions/with-set-prop-action"
-import { getNotifications, NotificationColumns } from "../../services/api/notification"
-import { id } from "date-fns/locale"
-interface Notification extends NotificationColumns {
-  isChecked?: boolean // 유저가 해당 notification 을 확인했는지 안했는지를 판단한다.
-  isDeleted?: boolean
-}
+import { NotificationColumns } from "../../services/api/notification"
+import { NotificationModel } from "../notification/notification"
 
 /**
  * 로그인한 사용자의 알림 목록
@@ -14,7 +9,7 @@ interface Notification extends NotificationColumns {
 
 export const NotificationStoreModel = types
   .model("NotificationStore")
-  .props({ notifications: types.frozen<Notification[]>([]) })
+  .props({ notifications: types.array(NotificationModel) })
   .views((self) => ({
     //* 안 읽은 알림 개수반환
     get unreadNotificationCount() {
@@ -34,47 +29,35 @@ export const NotificationStoreModel = types
     },
   })) // eslint-disable-line @typescript-eslint/no-unused-vars
   .actions((self) => ({
+    //* 새로운 notification 객체 추가
+    addNotification(value: NotificationColumns) {
+      self.notifications.push({
+        ...value,
+        isChecked: false,
+        isDeleted: false,
+      })
+    },
     //* 서버에서 받아온 notification 객체들에 isChecked 프로퍼티를 추가하여 저장한다.
     //* MST가 비어있을 때만 사용한다.
     setNotifications(value: NotificationColumns[]) {
-      // 각 notification 객체에 isChecked 프로퍼티를 추가한다.
-      self.notifications = value.map((item) => ({
-        ...item,
-        isChecked: false,
-        isDeleted: false,
-      }))
-    },
-
-    //* 새 notification 객체를 추가한다.
-    addNotification(value: NotificationColumns) {
-      self.notifications = [
-        // 기존 notification 객체
-        ...self.notifications,
-        // 새로운 notification 객체
-        {
-          ...value,
-          isChecked: false,
-          isDeleted: false,
-        },
-      ]
+      value.forEach((item) => {
+        this.addNotification(item)
+      })
     },
     //* 모든 notification을 isDeleted 처리한다.
     deleteAllNotifications() {
-      self.notifications = self.notifications.map((item) => {
-        console.log("item.id, item.isDeleted >>>", item.id, item.isDeleted)
+      self.notifications.forEach((item) => {
         if (item.isDeleted === false) {
           item.isDeleted = true
         }
-        return item
       })
     },
     //* 확인하지 않았던 notification 객체를 확인한 상태로 바꾼다
     setIsChecked() {
-      self.notifications = self.notifications.map((item) => {
-        if ("isChecked" in item && item.isChecked === false) {
+      self.notifications.forEach((item) => {
+        if (item.isChecked === false) {
           item.isChecked = true
         }
-        return item
       })
     },
 
