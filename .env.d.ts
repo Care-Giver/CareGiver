ec1f8a7b666c33eef4ee01a8112d2b26eba0792b
hojkim77 <rlaghwnd4534@gmail.com>
2024-02-17 16:35:44 +0900

    MVP-69 chore: react-native-dotenv패키지 추가 및 환경 세팅

---
+++ .env.d.ts
@@ -0,0 +1,6 @@
+declare module "@env" {
+  export const NOTION_API_KEY: string
+  export const NOTION_DATABASE_ID: string
+  export const PORTONE_API_KEY: string
+  export const PORTONE_SECRET_KEY: string
+}
