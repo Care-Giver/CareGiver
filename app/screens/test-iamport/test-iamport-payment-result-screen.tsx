a00eac981fe471d2dafe8ff19b29a4598227900a
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2024-02-26 22:42:14 +1300

    Revert "Update: 실제 결제 기능 원상태로 복구"
    This reverts commit fba99e8923f6c2216201beed8b1de1aa48933e90.

--- app/screens/test-iamport/test-iamport-payment-result-screen.tsx
+++ app/screens/test-iamport/test-iamport-payment-result-screen.tsx
@@ -155,7 +155,7 @@ export const TestIamportPaymentResultScreen: FC<
           // navigation.navigate("test-iamport-screen")
         }
       >
-        <PreBol16 text={`${isSuccess ? "처음" : "이전 "} 화면으로 돌아가기`} color="white" />
+        <PreReg18 text="결제하기" />
       </TouchableOpacity>
     </Screen>
   )
