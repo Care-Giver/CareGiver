e232bf48bfa7d28b41c33eea321617526458208b
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2024-03-03 01:44:01 +1300

    Chore: 테스트용 코드 정리

--- app/screens/_CLIENT/search-stack/search-screen/search-screen.tsx
+++ app/screens/_CLIENT/search-stack/search-screen/search-screen.tsx
@@ -89,7 +89,6 @@ export const SearchScreen: FC<StackScreenProps<NavigatorParamList, "search-scree
 
     const {
       userStore: { cacheSearchRequest, cachedSearchRequest },
-      uiStore: { setCaution },
     } = useStores()
 
     //* 서비스 형태
@@ -277,22 +276,15 @@ export const SearchScreen: FC<StackScreenProps<NavigatorParamList, "search-scree
             {/* //? 날짜 선택 버튼 */}
             <RowRoundedButton
               onPress={() => {
-                // 테스트용
-                setCaution({
-                  title: "제목",
-                  message: "내용",
-                  image: "caution",
+                setIsDropdownOpen(false)
+                setIsCalendarOpen(!isCalendarOpen)
+                LayoutAnimation.configureNext(LayoutAnimation.create(170, "easeOut", "opacity"))
+                // TODO: 아이폰 디바이스에서 제대로 된 QA 필요함
+                scrollViewRef.current?.scrollTo({
+                  x: 0,
+                  y: 200,
+                  animated: true,
                 })
-
-                // setIsDropdownOpen(false)
-                // setIsCalendarOpen(!isCalendarOpen)
-                // LayoutAnimation.configureNext(LayoutAnimation.create(170, "easeOut", "opacity"))
-                // // TODO: 아이폰 디바이스에서 제대로 된 QA 필요함
-                // scrollViewRef.current?.scrollTo({
-                //   x: 0,
-                //   y: 200,
-                //   animated: true,
-                // })
               }}
               image={images.calendar}
               // text={date ? `${date?.dateString?.replace(/-/g, ".")}` : "날짜를 선택해주세요"} // 주의! replaceAll() 은 RN 에서 사용불가 (안드로이드에서 작동 불능 😂) - https://stackoverflow.com/q/69297024/16673541
