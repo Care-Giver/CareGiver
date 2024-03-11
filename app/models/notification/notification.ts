a3627b27974b5da1e64bdb992cdfc0a3ca5b456b
minSsan <msp1125@hanyang.ac.kr>
2024-03-11 23:21:52 +0900

    MVP-81 review: 코드리뷰 반영

--- app/models/notification/notification.ts
+++ app/models/notification/notification.ts
@@ -14,8 +14,8 @@ export const NotificationModel = types
     needToPush: types.boolean,
     adAtNight: types.boolean,
     //? maybe / maybeNull 중에 고민 (maybe는 undefined로, maybeNull은 null로 판단하는 것으로 이해했음)
-    careGiverReceiverId: types.maybe(types.number),
-    clientReceiverId: types.maybe(types.number),
+    careGiverReceiverId: types.maybeNull(types.number),
+    clientReceiverId: types.maybeNull(types.number),
     isChecked: types.boolean,
     isDeleted: types.boolean,
   })
