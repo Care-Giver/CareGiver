32c22374beda8fd018997e70c65dbfed87032104
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2024-03-16 01:04:38 +1300

    feat(place-holder-input-box): textInputProps 추가

--- app/components/_TEXT_INPUT/place-holder-input-box/place-holder-input-box.tsx
+++ app/components/_TEXT_INPUT/place-holder-input-box/place-holder-input-box.tsx
@@ -1,10 +1,10 @@
 import React from "react"
-import { StyleProp, ViewStyle, TextInput, StyleSheet, View } from "react-native"
+import { StyleProp, ViewStyle, TextInput, StyleSheet, View, TextInputProps } from "react-native"
 import { observer } from "mobx-react-lite"
 import { DISABLED, LBG } from "#theme"
 import { PRETENDARD_REGULAR } from "#fonts"
 
-export interface PlaceHolderInputBoxProps {
+export interface PlaceHolderInputBoxProps extends TextInputProps {
   /**
    * 추가적인 padding, margin 을 줌으로써, 위치를 조정할 수 있습니다.
    */
@@ -45,6 +45,7 @@ export const PlaceHolderInputBox = observer(function PlaceHolderInputBox(
     boxHeight = 78,
     backgroundColor = LBG,
     borderColor = LBG,
+    ...textInputProps
   } = props
   const text = props.text
   const setText = props.setText
@@ -65,6 +66,7 @@ export const PlaceHolderInputBox = observer(function PlaceHolderInputBox(
         value={text}
         onChangeText={setText}
         //*scrollEnabled={false}
+        {...textInputProps}
       />
     </View>
   )
