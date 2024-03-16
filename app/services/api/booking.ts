3b7189ac28285414dfdcf217917b8ea4fb2d94a7
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2024-03-16 18:02:03 +1300

    fix(getPreviousBookings): 에러시, 리턴 타입 빈 배열로수정

--- app/services/api/booking.ts
+++ app/services/api/booking.ts
@@ -445,7 +445,7 @@ export const getPreviousBookings = async (): Promise<PreviousBooking[]> => {
       const error = response.data.error
       console.error("[getPreviousBookings] error >>>", error)
       // @ts-ignore
-      return error
+      return []
     }
 
     // console.log("[getPreviousBookings] response.data >>> ", response.data)
